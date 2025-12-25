e85d783e8a3f24375c3a6b872c12361f0d064c84
Moulay <moulay@openmind-it.be>
2025-12-25 22:50:07 +0100

    application config : patch au lieu de put

--- src/services/applicationConfig.service.ts
+++ src/services/applicationConfig.service.ts
@@ -3,11 +3,11 @@ import type { ApplicationConfig } from "../types/api.types";
 
 export const applicationConfigService = {
   async getConfig(): Promise<ApplicationConfig> {
-    const { data } = await apiClient.get<ApplicationConfig>("/api/v1/application-config");
+    const { data } = await apiClient.get<ApplicationConfig>("/application-config");
     return data;
   },
   async updateConfig(payload: ApplicationConfig): Promise<ApplicationConfig> {
-    const { data } = await apiClient.put<ApplicationConfig>("/api/v1/application-config", payload);
+    const { data } = await apiClient.patch<ApplicationConfig>("/application-config", payload);
     return data;
   },
 };
